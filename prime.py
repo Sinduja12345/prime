906e42958046d831a7f953edcbf23a4c9fa3c928
Sinduja12345 <149670898+Sinduja12345@users.noreply.github.com>
2024-11-13 13:16:54 +0100

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 #find prime number
 
 for num in range(1, 251): 
-    if num > 1:  # Prime numbers are greater than 1
+    if num > 1: 
         for i in range(2, num): 
             if num % i == 0:  
                 break

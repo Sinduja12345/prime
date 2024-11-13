a438e88262793af1cc3d5c738bc0d21de3ccd7b0
Sinduja12345 <149670898+Sinduja12345@users.noreply.github.com>
2024-11-13 13:16:27 +0100

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,8 +2,8 @@
 
 for num in range(1, 251): 
     if num > 1:  # Prime numbers are greater than 1
-        for i in range(2, num):  # Check divisibility from 2 to num-1
-            if num % i == 0:  # If num is divisible by any number other than 1 and itself
+        for i in range(2, num): 
+            if num % i == 0:  
                 break
         else:
             print(num)
